3437167337fcfc55f2e68a3b5fe3de6a4688f872
pimentelmugica <javierpimentelmugica@hotmail.com>
2020-04-10 21:34:02 +0200

    Psychological inquiry feature

--- Test/features/inquiry.feature
+++ Test/features/inquiry.feature
@@ -1,9 +1,19 @@
 Feature: As a patient, I want to reach the inquiry form
 
-@Inquiry
+#@formInquiry
 Scenario: A patient is able to reach the inquiry form
   #Checking if a user is able to reach the inquiry form
 
   Given a user goes to the home page
   When the user clicks on 'Make a Inquiry' button
-  Then the inquiry form is shown           
+  Then the inquiry form is shown
+
+@psychologicalInquiry
+Scenario: A patient is able to send an inquiry
+  #Checking if a user is able send a psychological inquiry
+
+  Given a user goes to the home page
+  When the user clicks on 'Make a Inquiry' button
+  And the user fills the psychological form
+  And the user clicks on 'Send Inquiry' button
+  Then the inquiry will be sent           
--- Test/features/step_definitions/inquirySteps.js
+++ Test/features/step_definitions/inquirySteps.js
@@ -18,3 +18,27 @@ Then(/^the inquiry form is shown$/, async () => {
     .waitForElementVisible('header[class="register-form__header"]', 10000)
     .assert.elementPresent('form[action="#"]')
 });
+
+When(/^the user fills the psychological form$/, async () => {
+  await client
+    .setValue('input[name="age"]', '18')
+    .setValue('input[name="email"]', 'j.pimentelmugica+automation@gmail.com')
+    .setValue('input[name="confirmEmail"]', 'j.pimentelmugica+automation@gmail.com')
+    .click('div[aria-haspopup="listbox"]')
+    .click('ul[role="listbox"] li[tabindex="-1"]')
+    .setValue('textarea[name="summary"]', 'This is an automated message')
+    .click('input[name="privacy"]')
+    .click('input[name="terms"]')
+    .click('input[name="confirmAge"]')    
+});
+
+When(/^the user clicks on 'Send Inquiry' button$/, async () => {
+  await client
+    .waitForElementVisible('button[type="submit"]', 2000)
+    .click('button[type="submit"]')
+});
+
+Then(/^the inquiry will be sent$/, async () => {
+  await client
+    .waitForElementVisible('nav[class="back-home"]', 2000)
+});
